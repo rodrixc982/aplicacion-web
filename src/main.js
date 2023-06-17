4a887555a2100d1dce6f690dae37fac99b2648a5
rodrixc982 <cristian.rodrixc982@gmail.com>
2023-06-17 12:14:14 -0500

    Agregamos Taiwind y Element-plus

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
+//importar la libreria de element-plus
+import ElemenPlus from 'element-plus'
+import'element-plus/dist/index.css'
+//fin de la importacion de la libreria
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use (ElemenPlus)
+app.mount('#app')
