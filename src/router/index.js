b550d6525f01856a61130decb0c87ce1eedefb80
rodrixc982 <cristian.rodrixc982@gmail.com>
2023-06-17 17:27:48 -0500

    Agregamos Taiwind y Element-plus

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
+//importamos vue-router
 import { createRouter, createWebHistory } from 'vue-router'
+//importamos los componentes de las vistas
+import Inicio from '@/views/Inicio.vue'
+import Registro from '@/views/Registro.vue'
 
-import Home from '@/views/Home.vue'
-
+//creamos las rutas
 const routes = [
-    { path: '/', name: 'Home', component: Home },
+    {path: '/', name: 'Inicio', component: Inicio},
+    {path: '/registro', name: 'Registro', component: Registro},
 ]
 
+//creamos el router
 const router = createRouter({
-      history: createWebHistory(),
-      routes,
+    history: createWebHistory(),
+    routes,
 })
 
+//exportamos el router
 export default router
---
+++ src/views/Registro.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { reactive } from 'vue';
+
+const labelPosition = 'top';
+
+const formRegistro = reactive({
+    nombres: '',
+    apellidos: '',
+    correo: '',
+    contrasena: '',
+    confirmarContrasena: '',
+})
+</script>
+<template>
+    <div class="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img src="@/assets/vue.svg" alt="logo vue" class="mx-auto h-12 w-auto">
+            <h2 class="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Registrar usuario</h2>
+            <div class="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
+                <el-form 
+                :model="formRegistro"
+                :label-position="labelPosition"
+                >
+                    <el-row :gutter="10">
+                        <el-col :span="12">
+                            <el-form-item label="Nombres">
+                                <el-input v-model="formRegistro.nombres" placeholder="Nombres" required clearable></el-input>
+                            </el-form-item>
+                        </el-col>
+                        <el-col :span="12">
+                            <el-form-item label="Apellidos">
+                                <el-input v-model="formRegistro.apellidos" placeholder="Apellidos" required clearable></el-input>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+
+                </el-form>
+            </div>
+        </div>
+    </div>
+</template>
